f74aae2cf12204c40ee002c6830ef68cc6bd067d
Katja Stelmaszyk <katja.stelmaszyk@netformic.com>
2021-11-11 23:21:00 +0100

    bug fixing quantity field plugin

--- SwagThemeAdvanced/src/Resources/app/storefront/src/scripts/quantity-field.plugin.js
+++ SwagThemeAdvanced/src/Resources/app/storefront/src/scripts/quantity-field.plugin.js
@@ -1,10 +1,13 @@
 import Plugin from "src/plugin-system/plugin.class";
 import DomAccess from "src/helper/dom-access.helper";
+
 export default class QuantityFieldPlugin extends Plugin {
     init() {
         this.minus = DomAccess.querySelector(this.el, '.decrease')
         this.plus = DomAccess.querySelector(this.el, '.increase')
         this.field = DomAccess.querySelector(this.el, 'input[type="number"]')
+
+        console.log(this.options[0])
 
         this.registerEvents()
     }
@@ -14,18 +17,40 @@ export default class QuantityFieldPlugin extends Plugin {
         this.plus.addEventListener('click', this.increaseQuantity.bind(this))
     }
 
+    /**
+     * Reduce Quantity
+     */
     decreaseQuantity() {
         const step = parseInt(this.options.purchaseSteps)
         const newQuant = parseInt(this.field.value) - step
+
+        /**
+         * (re)set Quantity to Min-Value
+         */
         if(newQuant <= step) {
             this.field.value = step
             return
         }
+
+
         this.field.value = newQuant
     }
 
+    /**
+     * Add items
+     */
     increaseQuantity() {
-        this.field.value = parseInt(this.field.value) + parseInt(this.options.purchaseSteps)
+        const step = parseInt(this.options.purchaseSteps)
+        const newQuant = parseInt(this.field.value) - step
+        const maxQuantity = parseInt(this.options.maxQuantity)
+        /**
+         * (re)set Quantity to Max-Value
+         */
+        if(newQuant >= parseInt(maxQuantity)) {
+            this.field.value = maxQuantity
+            return
+        }
+        this.field.value = parseInt(this.field.value) + parseInt(step)
 
     }
 }
