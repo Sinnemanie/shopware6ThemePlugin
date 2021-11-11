82f0dc51c70eb539988eab88e08099678cc53232
Katja Stelmaszyk <katja.stelmaszyk@netformic.com>
2021-11-11 13:30:39 +0100

    code cleanup remove comments

--- SwagThemeAdvanced/src/Resources/app/storefront/src/scripts/rumble.js
+++ SwagThemeAdvanced/src/Resources/app/storefront/src/scripts/rumble.js
@@ -3,21 +3,12 @@ import DomAccess from "src/helper/dom-access.helper";
 import HttpClient from "src/service/http-client.service";
 
 // https://github.com/shopware/platform/issues/567
-
-
 export default class Rumble extends AddToCartPlugin {
     init() {
-        //console.info(window.PluginManager.getPluginInstancesFromElement(this.el))
-        //console.info(window.PluginManager.getPluginInstancesFromElement(this.el))
-
         this.PluginManager = window.PluginManager
         this._cartEL = DomAccess.querySelector(document, '.header-cart')
         this._client = new HttpClient(window.accessKey, window.contextToken)
         super.init()
-
-
-        //this._client = new HttpClient();
-        //this.fetchData();
     }
 
     _openOffCanvasCart(instance, requestUrl, formData) {
@@ -31,19 +22,8 @@ export default class Rumble extends AddToCartPlugin {
 
     _refreshCartValue() {
         const cartWidgetEL = DomAccess.querySelector(this._cartEL, '[data-cart-widget="true"]')
-        //console.log(cartWidgetEL)
         const cartWidgetInstance = this.PluginManager.getPluginInstancesFromElement(cartWidgetEL, 'CartWidget')
         cartWidgetInstance.fetch()
-        console.log('fetch');
-        /*
-        fetch() {
-            if (this.el.getAttribute('data-cart-widget') !== 'load') {
-                return;
-            }
-
-            super.fetch();
-        }*/
-
     }
 
     _rumbleButton() {
@@ -52,13 +32,5 @@ export default class Rumble extends AddToCartPlugin {
             this._cartEL.classList.remove('rumble')
         }, 3000)
     }
-
-
-/*    fetchData() {
-        this._client.get('/widgets/checkout/info', this.handleData);
-    }
-    handleData(response) {
-        console.log(response);
-    }*/
 
 }
